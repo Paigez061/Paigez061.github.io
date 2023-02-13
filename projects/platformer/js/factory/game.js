abe07f843faee1066bb83789e37d53f7b139320f
Paigez061 <Zering913@stcharles.k12.la.us>
2023-02-13 20:10:16 +0000

    update all

--- projects/platformer/js/factory/game.js
+++ projects/platformer/js/factory/game.js
@@ -31,7 +31,7 @@
         // create namespace on the game //
         game.opspark = {};
         game.opspark.init = function() {
-            game.stage.backgroundColor = '578744';
+            game.stage.backgroundColor = '384037';
             game.physics.startSystem(Phaser.Physics.ARCADE);
             game.physics.arcade.TILE_BIAS = 2;
         };
